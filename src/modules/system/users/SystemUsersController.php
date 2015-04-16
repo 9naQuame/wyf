21935ad26a9b70bdd537e38a383d3a19c91e67b2
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-04-16 08:59:50 +0000

    Updated the users system

--- src/modules/system/users/SystemUsersController.php
+++ src/modules/system/users/SystemUsersController.php
@@ -1,13 +1,27 @@
 <?php
 class SystemUsersController extends ModelController 
 {
-	public $listFields = array(
-	    ".users.user_id",
-	    ".users.user_name",
-	    ".users.first_name",
-	    ".users.last_name",
-	    ".roles.role_name"
-	);
-	
-	public $modelName = ".users";
+    public $listFields = array(
+        ".users.user_id",
+        ".users.user_name",
+        ".users.first_name",
+        ".users.last_name",
+        ".roles.role_name"
+    );
+
+    public $modelName = ".users";
+
+    public function setupListView() 
+    {
+        $this->listView->addOperation('reset', 'Reset Password');
+    }
+    
+    public function reset($params)
+    {
+        $user = reset($this->model->setQueryResolve(false)->getWithField('user_id', $params[0]));
+        $user['user_status'] = 2;
+        $this->model->setData($user);
+        $this->model->update('user_id', $params[0]);
+        Application::redirect($this->urlPath);
+    }
 }
--- src/modules/system/users/SystemUsersModel.php
+++ src/modules/system/users/SystemUsersModel.php
@@ -10,6 +10,14 @@ class SystemUsersModel extends ORMSQLDatabaseModel
             $this->datastore->data["password"] = md5($this->datastore->data["user_name"]);
         }
         unset($this->datastore->data['user_id']);
+        if($this->datastore->data['role_id'] == 1 && $_SESSION['role_id'] != 1)
+        {
+            return array(
+                'role_id' => array(
+                    'Non super users cannot assign super users'
+                )
+            );
+        }
     }
     
     public function preAddHook()
