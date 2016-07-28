9fff7f9234d452e2c9c93741eed75044eacfa071
Nana Kwame <9naquame@gmail.com>
2016-07-28 09:17:20 +0000

    extended utils class and added lib to include paths

--- lib/Utils.php
+++ lib/Utils.php
@@ -41,4 +41,93 @@ class Utils
         
         return floor(($end - $start) / 86400);
     }
+    
+    public static function unComma($number)
+    {
+        return str_replace(',', '', $number);
+    }
+       
+    public static function stringToTime($string, $hasTime = false)
+    {
+        if(preg_match("/(\d{2})\/(\d{2})\/(\d{4})(\w\d{2}:\d{2}:\d{2})?/", $string) == 0) return false;
+        $dateComponents = explode(" ", $string);
+
+        $decomposeDate = explode("/", $dateComponents[0]);
+        $decomposeTime = array();
+
+        if($hasTime === true)
+        {
+            $decomposeTime = explode(":", $dateComponents[1]);
+        }
+
+        return
+        strtotime("{$decomposeDate[2]}-{$decomposeDate[1]}-{$decomposeDate[0]}") +
+        ($hasTime === true ? ($decomposeTime[0] * 3600 + $decomposeTime[1] * 60 + $decomposeTime[2]) : 0);
+    }
+    
+    public static function stringToDatabaseDate($string, $hasTime = false)
+    {
+        $timestamp = self::stringToTime($string, $hasTime);
+        return date("Y-m-d", $timestamp);
+    }
+    
+    public function sentenceTime($time, $options = null)
+    {
+        $elapsed = time() - $time;
+
+        if($elapsed < 10)
+        {
+            $englishDate = 'now';
+        }
+        elseif($elapsed >= 10 && $elapsed < 60)
+        {
+            $englishDate = "$elapsed seconds";
+        }
+        elseif($elapsed >= 60 && $elapsed < 3600)
+        {
+            $minutes = floor($elapsed / 60);
+            $englishDate = "$minutes minutes";
+        }
+        elseif($elapsed >= 3600 && $elapsed < 86400)
+        {
+            $hours = floor($elapsed / 3600);
+            $englishDate = "$hours hour" . ($hours > 1 ? 's' : '');
+        }
+        elseif($elapsed >= 86400 && $elapsed < 172800)
+        {
+            $englishDate = "yesterday";
+        }
+        elseif($elapsed >= 172800 && $elapsed < 604800)
+        {
+            $days = floor($elapsed / 86400);
+            $englishDate = "$days days";
+        }
+        elseif($elapsed >= 604800 && $elapsed < 2419200)
+        {
+            $weeks = floor($elapsed / 604800);
+            $englishDate = "$weeks weeks";
+        }
+        elseif($elapsed >= 2419200 && $elapsed < 31536000)
+        {
+            $months = floor($elapsed / 2419200);
+            $englishDate = "$months months";
+        }
+        elseif($elapsed >= 31536000)
+        {
+            $years = floor($elapsed / 31536000);
+            $englishDate = "$years years";
+        }
+
+        switch($options['elaborate_with'])
+        {
+            case 'ago':
+                if($englishDate != 'now' && $englishDate != 'yesterday')
+                {
+                    $englishDate .= ' ago';
+                }
+                break;
+        }
+
+        return $englishDate;
+    }
 }
