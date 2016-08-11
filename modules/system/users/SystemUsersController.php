e7ffe17db4ae69810502e50257f1c99ca4585a6a
Nana Kwame <9naquame@gmail.com>
2016-08-11 15:17:25 +0000

    added reset paswword

--- modules/system/users/SystemUsersController.php
+++ modules/system/users/SystemUsersController.php
@@ -1,13 +1,23 @@
 <?php
 class SystemUsersController extends ModelController 
 {
-	public $listFields = array(
-	    ".users.user_id",
-	    ".users.user_name",
-	    ".users.first_name",
-	    ".users.last_name",
-	    ".roles.role_name"
-	);
-	
-	public $modelName = ".users";
+    public $modelName = ".users";
+    
+    public $listFields = array(
+        ".users.user_id",
+        ".users.user_name",
+        ".users.first_name",
+        ".users.last_name",
+        ".roles.role_name"
+    );
+        
+    public function reset($params)
+    {
+        $this->model->queryResolve = false;
+        $user = $this->model->getWithField2('user_id', $params[0]);
+        $user[0]['user_status'] = '2';
+        $this->model->setData($user[0]);
+        $this->model->update('user_id', $params[0]);
+        Application::redirect($this->urlPath);
+    }
 }
